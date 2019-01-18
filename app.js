d3f3a714aec0a350061ffee5fd57cc0683e209ee
Edoardo <edward.lunardi@gmail.com>
2019-01-18 20:58:28 +0100

    Removed svg package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const TextToSVG = require('text-to-svg');
 
 app.use(express.static('public'));
 
