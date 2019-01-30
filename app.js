71fb1be5070143ca80e1d1620e2b2ac8a8b8490e
Edoardo <edward.lunardi@gmail.com>
2019-01-30 23:27:01 +0100

    Finished project2 page

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/project1', function (req, res) {
     res.sendFile(__dirname + '/public/project1.html');
 });
 
+app.get('/project2', function (req, res) {
+    res.sendFile(__dirname + '/public/project2.html');
+});
+
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
